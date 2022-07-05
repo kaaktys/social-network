0337535decd749cc6aa0f98942942332543170fc
Kate <katok0709@gmail.com>
2022-07-05 22:30:32 +0300

    create tree

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://i.pinimg.com/736x/59/1e/08/591e0856855d48a4dc4ec5e123a10cff.jpg' />
       </header>
+      <nav className='nav'>
+        <div><a>Profile</a></div>
+        <div><a>Messages</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
+      </nav>
+      <div className='content'>
+        <img src="https://media.wired.com/photos/593261cab8eb31692072f129/master/pass/85120553.jpg"/>
+      </div>
     </div>
   );
 }
+
 
 export default App;
